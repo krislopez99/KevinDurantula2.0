d9694a2f07d1f78dabedd1d40060a01dbcf1c260
Kris Lopez <unknownlopez101@gmail.com>
2022-05-21 01:45:46 -0400

    pulled new source code

--- Command.py
+++
@@ -1,8 +0,0 @@
-class COMMAND:
-    CMD_MOVE = "CMD_MOVE"
-    CMD_ATTITUDE = "CMD_ATTITUDE"
-    CMD_POSITION = "CMD_POSITION"
-    CMD_CALIBRATION = "CMD_CALIBRATION"
-    
-    def __init__(self):
-        pass
